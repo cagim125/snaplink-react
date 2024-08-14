801e0155d8d5ec83775c893b918a0d756bb69c13
cagim125 <cagim125@gmail.com>
2024-08-14 14:52:03 +0900

    useNavigate(), Swal 적용

--- src/components/user/Register.js
+++ src/components/user/Register.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
 import styles from './Register.module.scss'
 import axios from 'axios';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
 
 export default function Register() {
+  const navigate = useNavigate();
+
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
@@ -19,8 +24,31 @@ export default function Register() {
     try{
       const response = await axios.post('/users/register', formData)
       console.log(response);
+
+      if(response.status === 200) {
+        Swal.fire({
+          title: "회원가입 성공",
+          text: "회원가입이 정상적으로 처리되었습니다.",
+          icon: "success"
+        })
+        navigate('/')
+      } 
+
     } catch (err) {
-      console.log(err);
+      console.log(err.response);
+      const res = err.response;
+      if(res.status === 409) {
+        Swal.fire({
+          title: "이미 존재하는 아이디입니다.",
+          text: "이미 존재하는 아이디 입니다.",
+          icon: "error"
+        })
+
+        setUsername('');
+        
+      }
+
+      
     }
   }
 
@@ -42,16 +70,25 @@ export default function Register() {
     return emailPattern.test(email);
   };
 
+  // const handleAlert = () => {
+  //   Swal.fire({
+  //     title: "회원가입 성공",
+  //     text: "회원가입이 정상적으로 처리되었습니다.",
+  //     icon: "success"
+  //   })
+  // }
+
 
   return (
     <div className={styles.container}>
       <div className={styles.item}>
         <div>
-          <input type='text' placeholder='아이디' onChange={(e) => setUsername(e.target.value)} />
+          <input type='text' placeholder='아이디' value={username}
+          onChange={(e) => setUsername(e.target.value)} />
         </div>
         <div>
           <input className={isActive ? styles.active : styles.inactive} 
-            type='password' placeholder='비밀번호' 
+            type='password' placeholder='비밀번호' value={password}
             onChange={(e) => setPassword(e.target.value)} />
         </div>
         <div>
@@ -65,6 +102,7 @@ export default function Register() {
           {isValid === false && <p style={{ color: 'red' }}>Invalid email format.</p>}
         </div>
         <button onClick={() => currentEmail()} style={{cursor:'pointer', border:'none', backgroundColor:'green', color:'white', padding:'10px'}}>이메일 인증</button>
+        {/* <button onClick={() => handleAlert()}>sweetalert2 테스트</button> */}
         <input type='button' value='회원가입' onClick={() => handleRegister()} />
       </div>
     </div>
